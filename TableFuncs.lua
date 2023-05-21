9d2b0fa3423d06c8d3aabab154773c04603ccb4c
AlternateYT <86079772+AlternateYT@users.noreply.github.com>
2023-05-20 23:04:54 -0400

    Create TableFuncs.lua

---
+++ TableFuncs.lua
@@ -0,0 +1,25 @@
+local tableMod = {}
+
+tableMod.find = function(array, value)
+	if value and array and type(array) == "table" then
+		for _, needle in pairs(array) do
+			if needle and needle == value then
+				return needle
+			end
+		end
+	end
+	return nil
+end
+
+tableMod.findk = function(array, key)
+	if key and array and type(array) == "table" then
+		for needle, _ in pairs(array) do
+			if needle and needle == key then
+				return needle
+			end
+		end
+	end
+	return nil
+end
+
+return tableMod
