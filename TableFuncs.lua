ea4ec10709dfed46d4efa80fac5d2ccff9d5fe21
AlternateYT <86079772+AlternateYT@users.noreply.github.com>
2023-05-21 08:30:15 -0400

    Added table.remove

--- TableFuncs.lua
+++ TableFuncs.lua
@@ -33,4 +33,16 @@ tableMod.findk = function(array, key)
 	return nil
 end
 
+tableMod.remove = function(array, index)
+	if index and array and type(array) == "table" then
+		for needle, _ in ipairs(array) do
+			if needle and needle == index then
+				array[needle] = nil
+			end
+		end
+		return array
+	end
+	return nil
+end
+
 return tableMod
