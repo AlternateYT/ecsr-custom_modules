efb67aeb3ec192d6330de18128897d8c856d40d5
AlternateYT <86079772+AlternateYT@users.noreply.github.com>
2023-05-21 08:15:15 -0400

    Update TableFuncs.lua

--- TableFuncs.lua
+++ TableFuncs.lua
@@ -11,6 +11,17 @@ tableMod.find = function(array, value)
 	return nil
 end
 
+tableMod.findv = function(array, value)
+	if value and array and type(array) == "table" then
+		for _, needle in pairs(array) do
+			if needle and needle == value then
+				return needle
+			end
+		end
+	end
+	return nil
+end
+
 tableMod.findk = function(array, key)
 	if key and array and type(array) == "table" then
 		for needle, _ in pairs(array) do
